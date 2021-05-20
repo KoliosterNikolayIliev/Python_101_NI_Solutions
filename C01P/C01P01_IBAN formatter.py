5c935fbff4a15a6be594e22381153c42164c986a
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-05-20 23:12:05 +0300

    Update C01P01_IBAN formatter.py

---
+++ C01P/C01P01_IBAN formatter.py
@@ -0,0 +1,25 @@
+def iban_formatter(iban):
+    leveled_out_input = ''.join(iban.split(' '))
+    substring = ''
+    formatted_iban = ''
+    counter = 0
+
+    for symbol in leveled_out_input:
+        substring += symbol
+        counter += 1
+
+        if len(substring) == 4:
+            substring += ' '
+            formatted_iban += substring
+            substring = ''
+
+        if counter == len(leveled_out_input):
+            formatted_iban += substring
+
+    return formatted_iban
+
+
+print(iban_formatter("BG80BNBG96611020345678") == "BG80 BNBG 9661 1020 3456 78")
+print(iban_formatter("BG80 BNBG 9661 1020 3456 78") == "BG80 BNBG 9661 1020 3456 78")
+print(iban_formatter("BG14TTBB94005362446381") == "BG14 TTBB 9400 5362 4463 81")
+print(iban_formatter("BG91UNCR70001864961754") == "BG91 UNCR 7000 1864 9617 54")
